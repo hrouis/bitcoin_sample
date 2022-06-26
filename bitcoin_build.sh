ace92953d43045a93897f08e2dffeb85b64d484d
hrouis <haythem.rouis@gmail.com>
2022-06-26 18:03:20 +0200

    [update] docker config

---
+++ bitcoin_build.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+cd docker 
+
+docker build -f Dockerfile_bitcoin_install -t bitcoin-install .
+docker build -f Dockerfile_bitcoin_alice -t bitcoin-alice-node .
+docker build -f Dockerfile_bitcoin_bob -t bitcoin-bob-node .
+docker build -f Dockerfile_btc_rpc -t bitcoin-explorer .
